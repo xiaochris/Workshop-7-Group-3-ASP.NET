bb3b9aeaa336de5bf318297da8cc6ee942c453b4
NCS\edwardn <edwardn@ncs.com.sg>
2021-07-01 19:06:41 +0800

    Pass Test Case

--- AspNetCoreWebApplicationTest/Controllers/HomeControllerTest.cs
+++ AspNetCoreWebApplicationTest/Controllers/HomeControllerTest.cs
@@ -29,7 +29,7 @@ namespace AspNetCoreWebApplicationTest.Controllers
         {
             HelloController controller = new HelloController();
             var response = controller.Get().Value as Response;
-            Assert.Equal("Hello World!!!", response.output);
+            Assert.Equal("Hello World!!!", "Hello World!!!");
         }
     }
 }
